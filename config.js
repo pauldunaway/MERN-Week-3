4f0d9995f741572366bc808a66c9dd7b8cb40f3c
Paul DUnaway <pauldunaway@yahoo.com>
2021-01-20 20:14:32 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '166694591545451',
+        clientSecret: '77cbf9930e6b916842d50cba05ed33cb'
+    }
 }
